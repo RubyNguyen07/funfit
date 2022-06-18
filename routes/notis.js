41fffd8158b45f07438b9e9791e28568dc8bc511
RubyNguyen07 <62697501+RubyNguyen07@users.noreply.github.com>
2022-06-18 10:22:30 +0700

    Core features of routines

--- routes/notis.js
+++ routes/notis.js
@@ -1,7 +1,20 @@
-// var express = require("express"); 
-// var router = express.Router(); 
+var express = require("express"); 
+var router = express.Router(); 
 
-// var notiController = require('../controllers/notiController');  
-// var middleware = require('../middlewares/auth'); 
+var notiController = require('../controllers/notiController');  
+var middleware = require('../middlewares/auth'); 
 
-// router.get(); 
+//Authorization 
+router.use(middleware.auth);
+
+// Get all notis
+router.get('/getAllNotis', notiController.getAllNotis); 
+
+// Get a noti 
+router.get('/getNoti', notiController.getNoti);
+
+// Send a noti 
+router.post('/sendANoti', notiController.sendANoti);
+
+// Delete a noti 
+router.delete('/deleteNoti', notiController.deleteNoti);
