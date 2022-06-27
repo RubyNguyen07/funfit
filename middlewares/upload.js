82bf8066ee2abf7e34f0a56dc843b5e8264cbadf
RubyNguyen07 <62697501+RubyNguyen07@users.noreply.github.com>
2022-06-27 23:56:20 +0700

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -6,10 +6,10 @@ var storage = new GridFsStorage({
 	url: process.env.DB_URL,
 	options: { useNewUrlParser: true, useUnifiedTopology: true },
 	file: (req, file) => {
-		const match = ["image/png", "image/jpeg"];
-		if (match.indexOf(file.mimetype) === -1) {
-			throw new Error("File not in correct form"); 
-		}
+		// const match = ["image/png", "image/jpeg"];
+		// if (match.indexOf(file.mimetype) === -1) {
+		// 	throw new Error("File not in correct form"); 
+		// }
 		return {
 			bucketName: process.env.BUCKET,
 			filename: req.user.id
